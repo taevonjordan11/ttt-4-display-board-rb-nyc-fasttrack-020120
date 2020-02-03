a4988fa61b2dc0f786d08f34129a4d5f56e07789
agile-proc-6339 <taevonjordan1@gmail.com>
2020-02-03 23:35:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" ", " ", " ", " ", " ", " ", " "," ","x "]
+board = [" ", " ", " ", " ", " ", " ", " "," ","x"]
 
 def display_board(board)
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
  puts "-----------"
- puts "   |   |   "
+ puts "   |   |"x""
 end
 
 puts display_board(board)
